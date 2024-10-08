9311ba2b7b10dc6d31d8a3ede84a4d32d61c514d
EGOR <a666@iMac-666.Dlink>
2024-10-08 18:11:03 +0300

    From A to B

---
+++ part2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void print_ab(int a, int b)
+{
+    if (a == b)
+    {
+        printf("%d", a);
+    }
+    else
+    {
+        (a < b) ? printf("%d ", a++) : printf("%d ", a--);
+        print_ab(a, b);
+    }
+}
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    print_ab(a, b);
+    return 0;
+}
